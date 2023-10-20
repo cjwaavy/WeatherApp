d52925800b12654fdbaccecfaa78cb6a8de9a334
cjmcc <cjmcclasky@gmail.com>
2023-10-19 23:34:05 -0500

    most recent commit

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
 import React, {useEffect,useState} from "react"
 import Tabs from "./src/components/Tabs"
-import { ActivityIndicator, SafeAreaView,View, Text, StyleSheet } from "react-native"
-import * as Location from 'expo-location'
-import UpcomingWeather from "./src/screens/UpcomingWeather"
-import {WEATHER_API_KEY} from "@env"
+import { ActivityIndicator, View, StyleSheet } from "react-native"
 import { useGetWeather } from "./src/hooks/useGetWeather"
-import { NavigationContainer } from "@react-navigation/native"
-import { WeatherType } from "./src/utilities/WeatherType"
+import ErrorItem from "./src/components/ErrorItem"
 const App = () => {
   const[weather, loading, errorMsg] = useGetWeather()
-  if (weather && weather.list){
-    console.log(weather)
+  if (weather && weather.list && !loading){
     return(
+      // <ErrorItem></ErrorItem>
       <Tabs weather = {weather}/>
-      // <SafeAreaView>
-      //   <Text>{JSON.stringify(weather)}</Text>
-      // </SafeAreaView>
     )
   }
-  // console.log(weather, loading, errorMsg)
   
   return (
       <View style={styles.container}>
-      <ActivityIndicator size="large" color="blue"/>
+      {errorMsg ? (<ErrorItem />):
+      (<ActivityIndicator size="large" color="blue"/> )
+      }
       </View>
       
     // <UpcomingWeather />
